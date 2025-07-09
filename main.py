040266b4e118dc69e2e627513a2888077775c088
Pembevich <vladpianist24@gmail.com>
2025-07-09 19:32:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
